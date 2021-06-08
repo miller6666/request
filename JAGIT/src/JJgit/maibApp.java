8939f17d7632be6ac507347b4533e286c268cc17
Bhong Chen <fktienmo@gmail.com>
2021-06-08 16:09:15 +0800

    MATH ADD

--- JAGIT/src/JJgit/maibApp.java
+++ JAGIT/src/JJgit/maibApp.java
@@ -5,7 +5,13 @@ public class maibApp {
 	public static void main(String[] args) {
 		//組長新增
 		System.out.println("join us");
-		
+		Math math = new Math();
+		System.out.println(math.add(10,8));
 	}
 
+}
+class Math{
+	int add(int a, int b) {
+		return a+b;
+	}
 }
